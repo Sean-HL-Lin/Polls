d84224e3a078eb0e5af1a4099285bc5b521e620e
Sean-HL-Lin <hongshenglin92@gmail.com>
2020-05-02 19:23:40 -0700

    20200502_sean_modify_admin_page

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
   # ex: /polls/5/
   path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-  path('<int:pk>/results/', views.ResultsView.as_view()), 
+  path('<int:pk>/results/', views.ResultsView.as_view(),  name='results'),
   path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
